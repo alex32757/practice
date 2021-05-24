4533e566293a9c4d63bf3a1bf827669740868860
Alexandr Panarin <alex32757@gmail.com>
2021-05-24 13:18:00 +0300

    add comments

--- Matlab/practise.m
+++ Matlab/practise.m
@@ -9,6 +9,7 @@ powerOfPoly = 4;
 N = power(2, powerOfPoly) - 1;
 mSeq = m_generator(polynom, registers, powerOfPoly, 0);
 countOfMeasurement = 2 * frequencyOfD + 1;
+%генерация шума
 valueBeforeSignal = zeros(1, countOfMeasurement);
 valueBeforeSignal = awgn(valueBeforeSignal, SNR);
 valueAfterSignal = zeros(1, countOfMeasurement);
@@ -18,12 +19,13 @@ for i = 1:N
     value = [value, I(2 + 1 / frequencyOfSignal * tau * (i - 1), 2 + 1 / frequencyOfSignal * tau * i, mSeq(i), frequencyOfSignal, frequencyOfD, amplitude)];
 end
 %value = [value, amplitude * power(-1, mSeq(N)) * sin(2 * pi * frequencyOfSignal * (2 + 1 / frequencyOfSignal * tau * N))];
+%наложение шума на сигнал
 value = awgn(value, SNR);
 resultSignal = [valueBeforeSignal, value, valueAfterSignal];
+%нормирование
 maxValueOfSignal = max(abs(resultSignal));
 resultSignal = resultSignal / maxValueOfSignal;
 t = 0:1/frequencyOfD:4+0.03;
 t = t(1:length(resultSignal));
-%t = [t, 0];
 plot(t, resultSignal)
 audiowrite('output.wav', resultSignal, frequencyOfD);
