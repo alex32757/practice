ff505839ee3fad906528f6a0fc33a32e96176c88
Alexandr Panarin <alex32757@gmail.com>
2021-05-24 12:58:29 +0300

    add matlab task

---
+++ Matlab/I.m
@@ -0,0 +1,4 @@
+function impulse = I(tStart, tEnd, sign, frequencyOfSignal, frequencyOfDiscret, a)
+    time = tStart: 1 / frequencyOfDiscret : (tEnd - (1 / frequencyOfDiscret));
+    impulse = a * power(-1, sign) * sin(2 * pi * frequencyOfSignal * time);
+end
---
+++ Matlab/m_generator.m
@@ -0,0 +1,10 @@
+%registers = [d0, d1, d2, d3], polynom = [c0, c1, c2, c3, c4]
+function m_seq = m_generator(polynom, registers, n, k)
+    for i = n:(power(2, n) - 2)
+        registers(end + 1) = 0;
+        for j = 1:n
+            registers(end) = xor(registers(end), polynom(j + 1) * registers(i - j + 1));
+        end
+    end
+    m_seq = registers;
+end
